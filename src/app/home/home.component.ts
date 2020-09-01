104dfdc33a0817f19ec51c1a1b5063770c0b2df7
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-01 18:15:41 +0300

    search working on local

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { ResultComponent } from './../result/result.component';
 import { HttpService } from './../http.service';
-import { Repository } from './../repository';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -11,7 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public repoService:HttpService , result:ResultComponent) { }
+  // user: any ;
+  // repos: any [];
+  // username: any
+ 
+
+  constructor(public httpService:HttpService , result:ResultComponent) { }
+  // findUser(){
+  //   this.httpService.updateUser(this.username);
+   
+  //   this.httpService.getUser().subscribe (response=>{
+  //    console.log (response)
+  //     this.user = response
+  //    });
+  //   this.httpService.getRepo().subscribe(repos=>{
+  //     console .log (repos)
+  //     this.repos = [repos]
+  //   });
+  
+  // }
+  // findRepo(){
+  //  this.httpService.updateUser(this.username);
+  //  this.httpService.getRepo().subscribe(repos=>{
+  //    console .log (repos)
+  //    this.repos = [repos]
+  //  });
+ 
+   
+
+  // }
 
   ngOnInit(): void {
   }
